af35ef12d00ebffc64692487a55349a4ef4f9d72
olivia pasion <oap9820@gmail.com>
2022-09-28 15:14:05 -0700

    built out UserContext.js

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,2 +1,15 @@
-import { useContext, useState } from 'react';
+import { createContext, useState } from 'react';
+import { getUser } from '../services/auth';
 
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser);
+
+  return <UserContext.Provider value={{ user, setUser}}>
+    {children}
+  </UserContext.Provider>
+};
+
+export { UserProvider, UserContext };
